d7b3665f036aff945e5b29f8e99dbe20599f68ba
Brian Thomas <brian.thomas@btechnical-consulting.com>
2019-03-30 13:17:03 -0500

    Auto start firefox with coverage stats

--- do_TestCoverage.sh
+++ do_TestCoverage.sh
@@ -2,5 +2,6 @@
 cd cmake-build-debug
 ctest
 cd -
-lcov --capture --no-external --directory ../DataMapper/ --output-file coverage.info
+lcov --capture --no-external --directory ../OrderManager-v1/ --output-file coverage.info
 genhtml coverage.info --output-directory out
+firefox out/index.html &
